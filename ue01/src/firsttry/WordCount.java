e22797a8a34283b2fd20bfada5606135c65b7610
Jacob Schrott <0062@htl.rennweg.at>
2023-09-19 11:24:26 +0200

    alle testcases funktionieren, ein paar änderungen, auskommentierte lines gelöscht

--- ue01/src/firsttry/WordCount.java
+++ ue01/src/firsttry/WordCount.java
@@ -9,13 +9,11 @@ public class WordCount {
         long count = 0;
         boolean isWord = false;
 
-        System.out.println(s);
         for (int i = 0; i < s.length(); i++) {
             if(s.charAt(i) == '"' && s.charAt(i-1) == '\\'){
                 s = s.replace(s.substring(i-1, i+1), "");
             }
         }
-        System.out.println(s);
         for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) == '"') {
                 int tagEnd = s.indexOf('"', i + 1);
@@ -29,14 +27,9 @@ public class WordCount {
             }
         }
         int end = s.length() - 1;
-        System.out.println(s);
         for (int i = 0; i < s.length(); i++) {
-            //System.out.println(i+" "+s.charAt(i));
-            //System.out.println(i+" "+isWord);
             if (Character.isLetter(s.charAt(i)) && i != end) {
                 isWord = true;
-                //System.out.println(s.charAt(i));
-                //System.out.println(i+" "+s.charAt(i));
             } else if (s.charAt(i) == '<' && s.indexOf('>', i) > s.indexOf('<', i + 1) && s.indexOf('<', i + 1) != -1) {
                 if (isWord) {
                     count++;
@@ -51,7 +44,6 @@ public class WordCount {
             } else if (s.charAt(i) == '<' && s.indexOf('>', i) != -1) {
                 if (isWord) {
                     count++;
-                    //System.out.println(i+" "+s.charAt(i));
                     isWord = false;
                 }
                 int tagEnd = s.indexOf('>', i);
@@ -59,24 +51,18 @@ public class WordCount {
             } else if (s.charAt(i) == '<' && s.indexOf('>', i) == -1) {
                 if (isWord) {
                     count++;
-                    //System.out.println(i+" "+s.charAt(i));
                     isWord = false;
                 }
                 break;
             } else if (!Character.isLetter(s.charAt(i)) && isWord) {
                 count++;
-                //System.out.println(isWord);
-                //System.out.println(i+" "+s.charAt(i));
                 isWord = false;
             } else if (Character.isLetter(s.charAt(i)) && i == end) {
                 count++;
-                //System.out.println(i+" "+s.charAt(i));
             } else if (s.charAt(i) == '<' && s.indexOf('>', i) == -1) {
                 i++;
             } else if (!Character.isLetter(s.charAt(i)) && isWord) {
                 count++;
-                //System.out.println(isWord);
-                //System.out.println(i+" "+s.charAt(i));
                 isWord = false;
             } else if (Character.isLetter(s.charAt(i)) && i == end) {
                 count++;
