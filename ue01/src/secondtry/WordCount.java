dcff0a94b59361a9e6b8c69b020bd0e7adadd6a4
Jacob Schrott <0062@htl.rennweg.at>
2023-09-25 16:44:52 +0200

    ue02 secondtry: alles funktioniert + html file ausgelesen: 470528

--- ue01/src/secondtry/WordCount.java
+++ ue01/src/secondtry/WordCount.java
@@ -1,7 +1,21 @@
 package secondtry;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
 public class WordCount {
     public static void main(String[] args) {
+        ///read all lines in a html file
+        File file = new File("C:\\Users\\jacob\\OneDrive - HTL Wien 3 Rennweg\\SJ2324\\SEW\\sew4_sem1j\\ue02\\src\\crsto12.html");
+        String text;
+        try {
+            text = Files.readString(file.toPath());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println(count(text));
+
         System.out.println(count(" eins<img alt=\"<bild\" keinwort>zwei"));
     }
 
@@ -10,11 +24,13 @@ public class WordCount {
         INWORD,
         INTAG,
         VERSCHACHTELT,
-        INSTRING
+        INSTRINGTAG,
+        BACKSLASH
     }
 
     public static int count(String text) {
-        if (text == null || text.isEmpty()) {
+        text = text.replace("\\\"", "");
+        if (text.isEmpty()) {
             return 0;
         }
 
@@ -23,7 +39,7 @@ public class WordCount {
 
         for (int i = 0; i < text.length(); i++) {
             switch (state) {
-                case INSTRING -> {
+                case INSTRINGTAG -> {
                     switch (text.charAt(i)) {
                         case '<' -> i++;
                         case '"' -> state = State.INTAG;
@@ -38,7 +54,8 @@ public class WordCount {
                     switch (text.charAt(i)) {
                         case '<' -> state = State.VERSCHACHTELT;
                         case '>' -> state = State.NOWORD;
-                        case '"' -> state = State.INSTRING;
+                        case '"' -> state = State.INSTRINGTAG;
+                        case '\\' -> state = State.BACKSLASH;
                     }
                 }
                 case NOWORD -> {
