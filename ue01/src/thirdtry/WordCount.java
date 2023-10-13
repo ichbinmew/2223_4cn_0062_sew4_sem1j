ad6f37f68306b3bbc44c4f451f270a8eab1eba15
Jacob Schrott <0062@htl.rennweg.at>
2023-10-13 15:17:19 +0200

    UE02 mit abstract: normal funktioniert

--- ue01/src/thirdtry/WordCount.java
+++ ue01/src/thirdtry/WordCount.java
@@ -1,19 +1,76 @@
 package thirdtry;
 
+import java.util.Objects;
+
 public class WordCount {
     public static void main(String[] args) {
-
+        System.out.println(count("ein:erster.Text"));
     }
     public enum State {
-        START {
+        NOWORD {
             @Override
-            public State process(char c, int counter) {
-                return null;
+            public State process(char c) {
+                if (c == ' '){
+                    return NOWORD;
+                } else if (c == '<'){
+                    return INTAG;
+                } else {
+                    if(Character.isAlphabetic(c)){
+                        return NEWWORD;
+                    } else {
+                        return NOWORD;
+                    }
+                }
+            }
+        },
+        NEWWORD{
+            @Override
+            public State process(char c) {
+                if (c == ' '){
+                    return NOWORD;
+                } else if (c == '<'){
+                    return INTAG;
+                } else {
+                    return INWORD;
+                }
+            }
+        },
+        INWORD {
+            @Override
+            public State process(char c) {
+                if (!Character.isAlphabetic(c)){
+                    return NOWORD;
+                } else if (c == '<'){
+                    return INTAG;
+                } else {
+                    return INWORD;
+                }
+            }
+        },
+        INTAG {
+            @Override
+            public State process(char c) {
+                if (c == ' '){
+                    return NOWORD;
+                } else if (c == '<'){
+                    return INTAG;
+                } else {
+                    return INWORD;
+                }
             }
         };
-        public abstract State process(char c, int counter);
+        public abstract State process(char c);
     }
     public static long count (String s){
-        return 0;
+        long counter = 0;
+        State state = State.NOWORD;
+        for (int i = 0; i < s.length(); i++) {
+            state = state.process(s.charAt(i));
+
+            if (Objects.equals(state, State.NEWWORD)) {
+                counter++;
+            }
+        }
+        return counter;
     }
 }
